11f63a31a490052e7614d9034b6e635d2b4d0c37
Axel mac <axelhernandezperez23@gmail.com>
2025-11-10 10:52:55 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function App() {
+  return(
+    <div className='bg-stone-900 min-h-screen'>
+      <h1 className='text-white'>APA Asistente</h1>
+    </div>
+  )
+}
+
+
+export default App;
