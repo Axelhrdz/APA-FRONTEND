04c6d01f3886a414d80e8ecf67b1272ef2568ca6
Axelhrdz <axelhernandezperez23@gmail.com>
2026-05-19 20:42:06 -0600

    setting up dev branch

--- src/components/home/GuestHome.jsx
+++ src/components/home/GuestHome.jsx
@@ -25,7 +25,7 @@ const GuestHome = () => {
                     <br />
                     <br />
 
-                    <span className='font-normal text-5xl text-red-200'>production - dev merged</span>
+                    <span className='font-normal text-5xl text-red-200'>development</span>
                 </h1>
             </div>
             <div id='description-div'>
